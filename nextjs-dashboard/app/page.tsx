ca3083becbcda6484c476fba35bb2b6145c2ad20
PedalConny <pedalconny@hotmail.com>
2024-04-22 15:03:46 +0200

    added fetchRevenue

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,3 +1,5 @@
+/* import styles from '@/app/ui/home.module.css';
+<div className={styles.shape}/>; */
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
