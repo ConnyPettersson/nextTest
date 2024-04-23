894f9f651d51e1c19e9efe70f9a2e5c9bf1ee54b
PedalConny <pedalconny@hotmail.com>
2024-04-23 16:50:23 +0200

    added search and pagination

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -1,10 +1,15 @@
 import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import { 
+  RevenueChartSkeleton, 
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+} from '@/app/ui/skeletons';
 
 export default async function Page() {
   const {
@@ -34,6 +39,9 @@ export default async function Page() {
        </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
         <LatestInvoices />
+        </Suspense>
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper  />
         </Suspense>
       </div>
     </main>
